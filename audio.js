e265549899090dc2e2698db9ee2189cd91fef44e
coolcodehere <Bushfries@yahoo.com>
2019-01-03 22:44:02 -0600

    first

---
+++ audio.js
@@ -0,0 +1,13 @@
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+
+    this.sound.play();
+
+  }
+
+  sound('./gnome.mp3')
